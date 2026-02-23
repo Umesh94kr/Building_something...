74436acb1300222a292e69c2cbad582af02eda56
Umesh94kr <umeshkumar94628@gmail.com>
2026-02-24 00:18:04 +0530

    good idea but latency died

--- main.py
+++ main.py
@@ -10,20 +10,27 @@ llm = ChatOllama(
     temperature=0.2
 )
 
+from pydantic import BaseModel, Field
+from typing import Annotated
+
+class StructuredOutput(BaseModel):
+    clean_voice : Annotated[str, Field(description="Raw voice input cleaned by LLM.")]
+
 if __name__ == "__main__":
     quest = invoke_ASR()
     # using LLM to correct words spoken by human
     template = "Correct the words spoken by user raw voice input : {raw_voice_input}, and you need to convert it to clean voice input of user.  " \
-    "Examples : quest -> i AM GOD, what are u doin" \
+    "Examples : Raw voice input -> i AM GOD, what are u doin" \
     "Clean voice input -> I am good, what are you doing?"
     prompt = PromptTemplate(
         template=template,
-        input_variables=['quest']
+        input_variables=['raw_voice_input']
     )
 
-    chain = prompt | llm 
-    question = chain.invoke(quest)
-    print(f"Question : {question}")
+    llm_str = llm.with_structured_output(StructuredOutput)
+    chain = prompt | llm_str
+    question = chain.invoke({'raw_voice_input' : quest})
+    print(f"Question : {question.clean_voice}")
 
     answer = "Hey Buddy! I'm doing good."
     invoke_TTS(answer)
