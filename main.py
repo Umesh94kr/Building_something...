bbd4703e888836a7bec976b9d42bbf44e078700c
Umesh94kr <umeshkumar94628@gmail.com>
2026-02-26 23:49:15 +0530

    Adding history

--- main.py
+++ main.py
@@ -16,29 +16,32 @@ from typing import Annotated
 # class StructuredOutput(BaseModel):
 #     clean_voice : Annotated[str, Field(description="Raw voice input cleaned by LLM.")]
 
+# function to find whether 'quit', 'exit', or 'end' like keywords are in the user question 
+def check_exit(user_question):
+    exit_words = ['quit', 'exit', 'end', 'stop', 'goodbye', 'bye']
+    user_words = user_question.split()
+    for word in user_words:
+        if word.lower() in exit_words:
+            return True 
+    return False
+
 if __name__ == "__main__":
-    quest = invoke_ASR()
-    # using LLM to correct words spoken by human
-    # template = "Correct the words spoken by user raw voice input : {raw_voice_input}, and you need to convert it to clean voice input of user.  " \
-    # "Examples : Raw voice input -> i AM GOD, what are u doin" \
-    # "Clean voice input -> I am good, what are you doing?"
-    # prompt = PromptTemplate(
-    #     template=template,
-    #     input_variables=['raw_voice_input']
-    # )
+    chat_history = []
+    user_question = invoke_ASR()
 
-    # llm_str = llm.with_structured_output(StructuredOutput)
-    # chain = prompt | llm_str
-    # question = chain.invoke({'raw_voice_input' : quest})
-    # print(f"Question : {question.clean_voice}")
+    chat_history.append("User question: " + user_question)
+    while check_exit(user_question) == False:
 
-    chat_template = "Given a user question, you need to answer it like a second human. user question : {user_question}"
+        chat_template = "Given a user_question and chat_history, you need to answer it like a second human. user question : {user_question}, chat_history : {chat_history}"
 
-    chat_prompt = PromptTemplate(
-        template=chat_template,
-        input_variables=['user_question']
-    )
+        chat_prompt = PromptTemplate(
+            template=chat_template,
+            input_variables=['user_question', 'chat_history']
+        )
 
-    chain = chat_prompt | llm 
-    response = chain.invoke({'user_question' : quest})
-    invoke_TTS(response.content)
+        chain = chat_prompt | llm 
+        response = chain.invoke({'user_question' : user_question, 'chat_history' : chat_history})
+        invoke_TTS(response.content)
+        chat_history.append("AI response: " + response.content)
+        user_question = invoke_ASR()
+        chat_history.append("User question: " + user_question)
